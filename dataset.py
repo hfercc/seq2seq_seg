06c6a8198cb65e8aed01d702fb104e8a84866997
ChanYH <hfercxx@hotmail.com>
2019-02-13 00:36:57 +0800

    p

--- dataset.py
+++ dataset.py
@@ -1,14 +1,22 @@
 from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import torchvision.transforms as transforms
 from glob import glob 
 import os.path as osp
 
+default_transforms = transforms.Compose([
+    transforms.Resize((256,448)),
+    transforms.ToTensor()
+])
+
 class YoutubeDataset(Dataset):
 
-    def __init__(self, seq_len=5, split="train", base_dir='/raid/sunfangwen/wzh/youtube/'):
+    def __init__(self, seq_len=5, split="train", base_dir='/raid/sunfangwen/wzh/youtube/', transforms = default_transforms):
 
         self.dir_list = open(split + "_dir.txt").readlines()
         self.base_dir = base_dir
         self.pathset = []
+        self.transforms = transforms
         for a in self.dir_list:
             j, m = a.replace("\n", "").split(' ')
             full_path_j = osp.join(self.base_dir, j, "*.png")
@@ -18,6 +26,21 @@ class YoutubeDataset(Dataset):
             for i in range(len(m_path) - seq_len + 1):
                 self.pathset.append((j_path[i:i + seq_len], m_path[i:i + seq_len]))
 
+    def __getitem__(self, idx):
+
+        j_path, m_path = self.pathset[idx]
+        j = []
+        m = []
+        for jp in j_path:
+            img = Image.open(jp)
+            img = self.transforms(img)
+            j.append(img)
+        for mp in m_path:
+            img = Image.open(mp)
+            img = self.transforms(img)
+            m.append(img)
+        return j, m
+
     def __len__(self):
         return len(self.pathset)
 
@@ -25,3 +48,6 @@ class YoutubeDataset(Dataset):
 if __name__ == '__main__':
     dataset = YoutubeDataset()
     print(len(dataset))
+    for d in dataset:
+        print(d)
+        break
